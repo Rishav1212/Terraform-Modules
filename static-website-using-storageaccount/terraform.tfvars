436104f94b3740105eaa66fd81f0297916647789
Rishav Chakraborty <71714237+Rishav1212@users.noreply.github.com>
2024-12-06 20:54:13 +0530

    Add files via upload

---
+++ static-website-using-storageaccount/terraform.tfvars
@@ -0,0 +1,5 @@
+location              = "India Central"
+resource_group_name   = "rg-static-website"
+storage_account_name  = "rishavstoragebucket"
+index_file_path       = "./index.html"
+error_file_path       = "./404.html"
---
+++ static-website-using-storageaccount/variables.tf
@@ -0,0 +1,20 @@
+variable "location" {
+  description = "Azure region for the resources"
+  default     = "India Central"
+}
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "rg-static-website"
+}
+variable "storage_account_name" {
+  description = "Globally unique name for the storage account"
+  default     = "rishavstoragebucket"
+}
+variable "index_file_path" {
+  description = "Path to the index.html file"
+  default     = "./index.html"
+}
+variable "error_file_path" {
+  description = "Path to the 404.html file"
+  default     = "./404.html"
+}
